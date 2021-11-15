b152a6098199ec08d316f4ad6f9c33ca4a234fe6
Oscar Hernandez <oscarh425@outlook.com>
2021-11-15 14:45:02 -0800

    Created a rough draft of a login page

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './Components/LoginForm'
 
 function App() {
+
+  const adminUser = {
+    email: 'admin@admin.com',
+    password: 'admin123'
+  }
+
+  const initialUser = {
+    name: '',
+    email: ''
+  }
+
+
+  const [user,setUser] = useState(initialUser);
+  const [error,setError] = useState('');
+
+  const submit = details => {
+    console.log(details);
+    if(details.email === adminUser.email && details.password===adminUser.password){
+      console.log('Success!')
+      setUser({
+        name: details.name,
+        email: details.email
+      })
+    } else {
+      console.log('Inputs Incorrect')
+    }
+  }
+
+  
+  const Logout = () => {
+    console.log('Logout');
+    setUser(initialUser);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != '') ? ( 
+        <div>
+          <h2>Hello <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm 
+        submit={submit} 
+        error={error}
+        />
+      )}
     </div>
+      
   );
 }
+
 
 export default App;
---
+++ watermyplants/src/Components/LoginForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+
+export default function LoginForm ({submit, error}) {
+    const initalDetails = {name: '', email: '', password: ''}
+    const [details, setDetails] = useState(initalDetails)
+
+
+    // declare onsubmit function
+    const onSubmit = event => {
+        event.preventDefault();
+        submit(details);
+    }
+    //declare onChange 
+    const onChange = event =>{
+        const {name,value} = event.target
+        setDetails({...details, [event.target.name]:value});
+      }
+    
+
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div className = 'container'>
+                <h2>Login</h2>
+                <div className = 'form'>
+                    <label>Name:
+                        <input 
+                        type='text'
+                        name='name'
+                        onChange={onChange}
+                        />
+                    </label>
+                </div>
+                <div className = 'form'>
+                    <label>Email:
+                        <input 
+                        type='email'
+                        name='email'
+                        onChange={onChange}
+                        />
+                    </label>
+                </div>
+                <div className = 'form'>
+                    <label>Password:
+                        <input 
+                        type='password'
+                        name='password'
+                        onChange={onChange}
+                        />
+                    </label>
+                </div>
+                <input type='submit' value='Click here to Login'/>
+            </div>
+        </form>
+    )
+}
