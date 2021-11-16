bb7a49fbc049125100ad177ee466054fc5ba9d0f
Oscar Hernandez <oscarh425@outlook.com>
2021-11-16 14:08:07 -0800

    Finished much of PlantForm.js

--- bw-water-my-plants/src/App.js
+++ bw-water-my-plants/src/App.js
@@ -2,14 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react'
 import SignUpForm from './Components/SignUpForm';
+import PlantForm from './Components/PlantForm';
 import axios from 'axios';
+import schema from './validation/formSchema';
+import { ValidationError } from 'yup';
+import * as yup from 'yup';
+
+//Delcaring const for PlantForm.js
+const initialFormValues = {
+  //Text inputs (name and species) for Plants in PlantForm.js
+  name:'',
+  species:'',
+  //checkbox for watering instructions 
+  water: false
+}
+
+const initialFormErrors = {
+  name:'',
+  species: '',
+  water: ''
+}
+
+const initialPlant = []
 
 function App() {
-
+  //States for Login
   const freshForm = {name:'', email:'', pwd:'', tos: false}
   const [users, setUsers] = useState([])
   const [form, setForm] = useState(freshForm)
 
+  //States for PlantForm
+  const [plant, setPlant] = useState(initialPlant)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+
+  //:::: 
   const onSubmit = () => {
     const newUser = {name: form.name.trim(), email: form.email.trim(), pwd: form.pwd, tos: form.tos}
     axios.post('https://reqres.in/api/users', newUser)
@@ -23,6 +50,35 @@ function App() {
   const onChange = (name, value) => {
     setForm({...form, [name]: value})
   }
+  ////Below here will be onChange and onSubmit functions for PlantForm.js
+  //::::::
+  //Input Change for props in PlantForm.js
+  const inputChange = (name,value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues, [name]:value
+    })
+  }
+
+  //validation 
+  const validate = (name, value) => {
+    yup.reach(schema,name).validate(value)
+    .then(() => setFormErrors({...formErrors, [name]: ''}))
+    .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
+  }
+
+  //submit form for props in PlantForm.js
+
+  const formSubmit = () => {
+    const newPlant = {
+      name: formValues.name.trim(),
+      species: formValues.species.trim(),
+      water: ['water'].filter(water => !!formValues[water])
+    }
+    // postNewPlant(newPlant);
+  }
+
+
   return (
     <div className="App">
       <SignUpForm
@@ -31,6 +87,12 @@ function App() {
         change={onChange}      
       />
       <br/>
+      <PlantForm 
+      values={formValues}
+      change={inputChange}
+      submit={formSubmit}
+      errors={formErrors}
+      />
       <h2>User List</h2>
       {users.map(user => {return(
         <div key={Math.random()}>
---
+++ bw-water-my-plants/src/Components/PlantForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default function PlantForm(props){
+    const {values, change, submit, errors} = props 
+
+    const onChange = event => {
+        const { name, value, checked, type } = event.target
+        const realValue = type === 'checkbox' ? checked : value;
+        change(name, realValue)
+    }
+
+    const onSubmit = event => {
+        event.preventDefault();
+        submit();
+    }
+
+    return (
+        <form className='form container' onSubmit={onSubmit}>
+            <div className= 'form-group inputs'>
+                <h2>Input Your Plants!</h2>
+                
+                {/* Text Inputs */}
+                <label> Plant Nick Name: 
+                    <input id='name-input'
+                        value={values.name}
+                        name='name'
+                        type='text'
+                        onChange={onChange}
+                    />
+                </label>
+                <label> Plant Species:
+                    <input id='species-input'
+                        value={values.species}
+                        name='species'
+                        type='text'
+                        onChange={onChange}
+                    />
+                </label>
+                {/* CheckBox - Does the plant require strict vs not strict watering schedule. i.e. cactus (doesnt need much water) vs other normal plant */}
+                <label> Does Plant Require Strict Watering Schedule? 
+                    <input 
+                        type='checkbox'
+                        name='water'
+                        onChange={onChange}
+                        checked={values.water}
+                    />
+                </label>
+            </div>
+        </form>
+    )
+}
